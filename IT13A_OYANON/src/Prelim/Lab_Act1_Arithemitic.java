e669718d14643de489d706044f44111e6f78b3c5
Ronabaldoza <oyanonjefferson@gmail.com>
2025-02-18 08:04:41 +0800

    java file
    Added HelloWorld class

--- IT13A_OYANON/src/Prelim/Lab_Act1_Arithemitic.java
+++ IT13A_OYANON/src/Prelim/Lab_Act1_Arithemitic.java
@@ -1,13 +1,28 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package Prelim;
 
-/**
- *
- * @author Admin
- */
 public class Lab_Act1_Arithemitic {
-    
+    public static void main(String[] org){
+        
+        
+        int numb1 = 10;
+        int numb2 = 6;
+        int numb3 = 4;
+        int numb4 = 3;
+        
+       
+        
+        int oyanon1 = numb1 * numb3 + numb2;
+        int depensa1 = numb1 - numb3;
+        int oyanon2 = depensa1 % numb2;
+        int depensa2 = numb1 + numb3 + numb2;
+        int oyanon3 = depensa2 / numb4;
+        int depensa3 = numb3 * numb3;
+        int oyanon4 = numb1 * numb2 - depensa3;
+        
+          System.out.println("10 * 4 + 6 ="+oyanon1);
+         System.out.println("(10-4) % 6 = "+oyanon2);
+            System.out.println("(10 + 4 + 6)/3 = "+oyanon3);
+               System.out.println("10 * 6 -(4 * 4 ) = "+oyanon4);
+               
+          
+}
 }
